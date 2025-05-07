18ec05b7a4db2b5e6e473470415645462d8cfb3e
peilerx <peiler.dany@gmail.com>
2025-05-07 12:30:19 +0300

    default vulkan structures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+struct Swapchain {} //vulkan swapchain resources 
+
+struct Pipeline {} //vulkan pipeline resources
+
+struct Render {} //render resources
+
+struct App {} //basic init vulkan resources
+
 fn main() {
     println!("Hello, ash!");
 }
